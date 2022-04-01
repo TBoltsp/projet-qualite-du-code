6b85ac297a10991ffa5ecc44298729b7cc44a819
TBoltsp <sylvain.paulet01@gmail.com>
2022-04-01 11:33:39 +0200

    tests equals dates

--- TDD_petsitter/src/dev_test1/TDD_tests_petsitters.java
+++ TDD_petsitter/src/dev_test1/TDD_tests_petsitters.java
@@ -4,11 +4,18 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.time.LocalDateTime;
 import org.junit.jupiter.api.Test;
 
+import testing.Animal;
+import testing.Booking;
+import testing.Gardien;
+import testing.PlageHoraire;
+import testing.Proprietaire;
+import testing.Race;
+
 class TDD_tests_petsitters {
 
 	@Test
 	void test1() {
-		// La 'Demande de Booking' est refusée si le 'Gardien' n'a pas 'chien' dans la liste d'animaux acceptés.
+		// La 'Demande de Booking' est refusee si le 'Gardien' n'a pas 'chien' dans la liste d'animaux acceptes.
 		Animal A_rex = new Animal("rex", Race.CHIEN);
 		
 		Proprietaire P_joe = new Proprietaire("joe");
@@ -26,8 +33,8 @@ class TDD_tests_petsitters {
 	
 	@Test
 	void test2() {
-		//La 'Demande de Booking' est acceptée si le 'Gardien' a le 'chien' dans la liste d'animaux acceptés.
-		//A ce moment là , le 'Calendrier' du 'Gardien' est modifié, et la date (plage de date, début et fin) est marquée comme occuppée.
+		//La 'Demande de Booking' est acceptee si le 'Gardien' a le 'chien' dans la liste d'animaux acceptes.
+		//A ce moment la, le 'Calendrier' du 'Gardien' est modifie, et la date (plage de date, debut et fin) est marquee comme occuppee.
 		Animal A_rex = new Animal("rex", Race.CHIEN);
 		
 		Proprietaire P_joe = new Proprietaire("joe");
@@ -45,7 +52,7 @@ class TDD_tests_petsitters {
 	
 	@Test
 	void test3() {
-		//La 'Demande de Booking' est refusée si le 'Gardien' n'est pas disponible à  la date demandée en fonction de son 'Calendrier'
+		//La 'Demande de Booking' est refuse si le 'Gardien' n'est pas disponible a la date demandee en fonction de son 'Calendrier'
 		Animal A_rex = new Animal("rex", Race.CHIEN);
 		
 		Proprietaire P_joe = new Proprietaire("joe");
@@ -61,6 +68,40 @@ class TDD_tests_petsitters {
 		dateDebut = LocalDateTime.of(2022, 04, 01, 8, 0, 0);
 		dateFin = LocalDateTime.of(2022, 04, 01, 9, 30, 0);
 		assertFalse(Booking.demandeBooking(P_joe, G_helena, dateDebut, dateFin));
+	}
+	
+	@Test
+	void testPlageHoraire1() {
+		// redefinir equals de PlageHoraire sur les champs LocalDateTime uniquement
+		LocalDateTime dateDebut = LocalDateTime.of(2022, 04, 01, 10, 30, 0);
+		LocalDateTime dateFin = LocalDateTime.of(2022, 04, 01, 15, 30, 0);
+		
+		PlageHoraire ph1 = new PlageHoraire(dateDebut, dateFin);
+		PlageHoraire ph2 = new PlageHoraire(dateDebut, dateFin);
+		
+		assertTrue(ph1.equals(ph2));
+	}
+	
+	@Test
+	void testPlageHoraire2() {
+		// not equals si un element d'une date est different
+		LocalDateTime dateDebut = LocalDateTime.of(2022, 04, 01, 10, 30, 0);
+		LocalDateTime dateFin = LocalDateTime.of(2022, 04, 01, 15, 30, 0);
+		PlageHoraire ph1 = new PlageHoraire(dateDebut, dateFin);
+
+		dateFin = LocalDateTime.of(2022, 04, 01, 14, 30, 0);
+		PlageHoraire ph2 = new PlageHoraire(dateDebut, dateFin);
+		
+		assertFalse(ph1.equals(ph2));
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	void testPlageHoraire3() {
+		// trows IllegalArgumentException quand dateDebut superieur a dateFin
+		LocalDateTime dateDebut = LocalDateTime.of(2022, 04, 01, 10, 30, 0);
+		LocalDateTime dateFin = LocalDateTime.of(2022, 04, 01, 19, 30, 0);
+		
+		new PlageHoraire(dateDebut, dateFin);
 	}
 
 }
--- TDD_petsitter/src/testing/TDD_tests_petsitters.java
+++ TDD_petsitter/src/testing/TDD_tests_petsitters.java
@@ -10,26 +10,18 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class TDD_tests_petsitters {
-
-	private Proprietaire P_joe;
-	private Animal A_rex;
-	private Animal A_bob;
-	private Gardien G_helena;
-	private Calendrier calendrier;
-	private LocalDateTime dateDebut;
-	private LocalDateTime dateFin;
 	
 	@Test
 	void test1() {
-		// La 'Demande de Booking' est refusée si le 'Gardien' n'a pas 'chien' dans la liste d'animaux acceptés.
-		A_rex = new Animal("rex", Race.CHIEN);
+		// La 'Demande de Booking' est refusee si le 'Gardien' n'a pas 'chien' dans la liste d'animaux acceptes.
+		Animal A_rex = new Animal("rex", Race.CHIEN);
 		
-		P_joe = new Proprietaire("joe");
+		Proprietaire P_joe = new Proprietaire("joe");
 		P_joe.ajoutAnimal(A_rex);
 		
-		dateDebut = LocalDateTime.of(2022, 04, 01, 10, 30, 0);
-		dateFin = LocalDateTime.of(2022, 04, 01, 15, 30, 0);
-		G_helena = new Gardien("helena");
+		LocalDateTime dateDebut = LocalDateTime.of(2022, 04, 01, 10, 30, 0);
+		LocalDateTime dateFin = LocalDateTime.of(2022, 04, 01, 15, 30, 0);
+		Gardien G_helena = new Gardien("helena");
 		G_helena.ajoutRace(Race.CHAT);
 		G_helena.ajoutRace(Race.CHIEN);
 		G_helena.ajoutCreneaux(dateDebut, dateFin);
@@ -39,16 +31,16 @@ class TDD_tests_petsitters {
 	
 	@Test
 	void test2() {
-		//La 'Demande de Booking' est acceptée si le 'Gardien' a le 'chien' dans la liste d'animaux acceptés.
-		//A ce moment là, le 'Calendrier' du 'Gardien' est modifié, et la date (plage de date, début et fin) est marquée comme occuppée.
-		A_rex = new Animal("rex", Race.CHIEN);
+		//La 'Demande de Booking' est acceptee si le 'Gardien' a le 'chien' dans la liste d'animaux acceptes.
+		//A ce moment la, le 'Calendrier' du 'Gardien' est modifie, et la date (plage de date, debut et fin) est marquee comme occuppee.
+		Animal A_rex = new Animal("rex", Race.CHIEN);
 		
-		P_joe = new Proprietaire("joe");
+		Proprietaire P_joe = new Proprietaire("joe");
 		P_joe.ajoutAnimal(A_rex);
 		
-		dateDebut = LocalDateTime.of(2022, 04, 01, 10, 30, 0);
-		dateFin = LocalDateTime.of(2022, 04, 01, 15, 30, 0);
-		G_helena = new Gardien("helena");
+		LocalDateTime dateDebut = LocalDateTime.of(2022, 04, 01, 10, 30, 0);
+		LocalDateTime dateFin = LocalDateTime.of(2022, 04, 01, 15, 30, 0);
+		Gardien G_helena = new Gardien("helena");
 		G_helena.ajoutRace(Race.CHAT);
 		G_helena.ajoutRace(Race.CHIEN);
 		G_helena.ajoutCreneaux(dateDebut, dateFin);
@@ -58,15 +50,15 @@ class TDD_tests_petsitters {
 	
 	@Test
 	void test3() {
-		//La 'Demande de Booking' est refusée si le 'Gardien' n'est pas disponible à la date demandée en fonction de son 'Calendrier'
-		A_rex = new Animal("rex", Race.CHIEN);
+		//La 'Demande de Booking' est refuse si le 'Gardien' n'est pas disponible a la date demandee en fonction de son 'Calendrier'
+		Animal A_rex = new Animal("rex", Race.CHIEN);
 		
-		P_joe = new Proprietaire("joe");
+		Proprietaire P_joe = new Proprietaire("joe");
 		P_joe.ajoutAnimal(A_rex);
 		
-		dateDebut = LocalDateTime.of(2022, 04, 01, 10, 30, 0);
-		dateFin = LocalDateTime.of(2022, 04, 01, 15, 30, 0);
-		G_helena = new Gardien("helena");
+		LocalDateTime dateDebut = LocalDateTime.of(2022, 04, 01, 10, 30, 0);
+		LocalDateTime dateFin = LocalDateTime.of(2022, 04, 01, 15, 30, 0);
+		Gardien G_helena = new Gardien("helena");
 		G_helena.ajoutRace(Race.CHAT);
 		G_helena.ajoutRace(Race.CHIEN);
 		G_helena.ajoutCreneaux(dateDebut, dateFin);
@@ -74,6 +66,50 @@ class TDD_tests_petsitters {
 		dateDebut = LocalDateTime.of(2022, 04, 01, 8, 0, 0);
 		dateFin = LocalDateTime.of(2022, 04, 01, 9, 30, 0);
 		assertFalse(Booking.demandeBooking(P_joe, G_helena, dateDebut, dateFin));
+	}
+	
+	@Test
+	void testPlageHoraire1() {
+		// redefinir equals de PlageHoraire sur les champs LocalDateTime uniquement
+		LocalDateTime dateDebut = LocalDateTime.of(2022, 04, 01, 10, 30, 0);
+		LocalDateTime dateFin = LocalDateTime.of(2022, 04, 01, 15, 30, 0);
+		
+		PlageHoraire ph1 = new PlageHoraire(dateDebut, dateFin);
+		PlageHoraire ph2 = new PlageHoraire(dateDebut, dateFin);
+		
+		assertTrue(ph1.equals(ph2));
+	}
+	
+	@Test
+	void testPlageHoraire2() {
+		// not equals si un element d'une date est different
+		LocalDateTime dateDebut = LocalDateTime.of(2022, 04, 01, 10, 30, 0);
+		LocalDateTime dateFin = LocalDateTime.of(2022, 04, 01, 15, 30, 0);
+		PlageHoraire ph1 = new PlageHoraire(dateDebut, dateFin);
+
+		dateFin = LocalDateTime.of(2022, 04, 01, 14, 30, 0);
+		PlageHoraire ph2 = new PlageHoraire(dateDebut, dateFin);
+		
+		assertFalse(ph1.equals(ph2));
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	void testPlageHoraire3() {
+		// trows IllegalArgumentException quand dateDebut superieur a dateFin
+		LocalDateTime dateDebut = LocalDateTime.of(2022, 04, 01, 10, 30, 0);
+		LocalDateTime dateFin = LocalDateTime.of(2022, 04, 01, 19, 30, 0);
+		
+		new PlageHoraire(dateDebut, dateFin);
+	}
+	
+	@Test
+	void testPlageHoraireColision() {
+		//
+		LocalDateTime dateDebut = LocalDateTime.of(2022, 04, 01, 10, 30, 0);
+		LocalDateTime dateFin = LocalDateTime.of(2022, 04, 01, 15, 30, 0);
+		
+		PlageHoraire ph1 = new PlageHoraire(dateDebut, dateFin);
+		
 	}
 
 }
